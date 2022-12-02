f7157329c934ae65dfa97a1276dc9e2f086943ef
Young soo <lovepys01@naver.com>
2022-12-02 10:58:51 +0900

    [Silver V] Title: 소수 찾기, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1978. 소수 찾기/소수 찾기.py
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.py
@@ -1,17 +1,15 @@
 n = int(input())
 arr = list(map(int, input().split()))
-def isprime(n):
-    for i in range(2, n):
-        if n % i == 0:
-            return False # i로 나누어 떨어지면 소수가 아니므로 False 리턴
-    
-    return True # False가 리턴되지 않고 for문을 빠져나왔다면 소수이므로 True 리턴
 
 cnt=0
-for _ in arr:
-  if isprime(_):
-    cnt += 1
-  if _ == 1:
-    cnt -= 1
+for num in arr:
+  error = 0
+  if num>1:
+    for i in range(2, num):
+      if num%i==0:
+        error+=1
+    if error ==0:
+      cnt+=1
+        
   
 print(cnt)
