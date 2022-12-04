7823740bb634467b7c58beab86c0c61b11475d41
Young soo <lovepys01@naver.com>
2022-12-04 23:54:37 +0900

    [Silver V] Title: 색종이, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.py
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -5,12 +5,8 @@ for _ in range(n):
   x, y = map(int, input().split())
   for i in range(x, x+10):
     for j in range(y, y+10):
-      arr[i][j] = 1
-  
-for i in range(100):
-  for j in range(100):
-    if arr[i][j] == 1:
-      cnt+=1
-  
-    
+      if arr[i][j] == 0 and i<100 and j<100:
+        arr[i][j] += 1
+        cnt += 1
+
 print(cnt)
