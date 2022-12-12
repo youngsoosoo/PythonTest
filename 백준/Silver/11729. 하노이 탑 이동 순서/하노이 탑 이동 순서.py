bce07a818706f1db2103e49f38ca53c10e563443
Young soo <lovepys01@naver.com>
2022-12-12 23:03:43 +0900

    [Silver I] Title: 하노이 탑 이동 순서, Time: 876 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+
+def hanoi(n, start, end) :
+	if n == 1:
+		print(start, end)
+		return
+	hanoi(n-1, start, 6-start-end)
+	print(start, end)
+	hanoi(n-1, 6-start-end, end)
+	
+   
+n = int(input())
+print(2**n-1)
+hanoi(n, 1, 3)
