3e2994e40a8d999770634400c6e27f54525307f5
Young soo <lovepys01@naver.com>
2022-12-13 12:00:38 +0900

    [Bronze II] Title: 블랙잭, Time: 88 ms, Memory: 36848 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,13 +1,16 @@
-n, m = map(int, input().split())
-num = list(map(int, input().split()))
-l = len(num)
-ans = 0
-for i in range(0, l-2):
-    for j in range(i+1, l-1):
-        for k in range(j+1, l):
-            if(num[i] + num[j] + num[k] > m):
-                continue
-            else:
-                ans = max(ans ,num[i] + num[j] + num[k])
+import sys
+input = sys.stdin.readline
+	
 
-print(ans)
+n, m = map(int, input().split())
+b = list(map(int, input().split()))
+a=[]
+for i in range(n):
+	for j in range(i+1, n):
+		for k in range(j+1, n):
+			if b[i] + b[j] + b[k]>m:
+				continue
+			else:
+				a.append(b[i] + b[j] + b[k])
+
+print(max(a))
