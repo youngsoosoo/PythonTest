46de43f48fd3c6b546f39a03168f44cead601db7
Young soo <lovepys01@naver.com>
2022-12-25 00:44:54 +0900

    [Silver III] Title: N과 M (2), Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+a=1
+def dfs(a):
+	if len(s) == m:
+		print(*s)
+		return
+	for i in range(a, n+1):
+		if i not in s:
+			s.append(i)
+			dfs(i)
+			s.pop()
+
+s=list()
+n, m = map(int, input().split())
+
+dfs(a)
