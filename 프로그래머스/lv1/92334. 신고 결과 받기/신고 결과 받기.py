26db58636a6b02858a6ce1fcdaa633c443937a8c
Young soo <lovepys01@naver.com>
2023-03-02 23:52:10 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -0,0 +1,19 @@
+def solution(id_list, report, k):
+    answer = [0 for i in id_list]
+    report = set(report)
+    report = list(report)
+    dic = {i:[] for i in id_list}
+    graph = [set() for i in range(len(id_list))]
+    for i in range(len(report)):
+        report[i] = list(report[i].split())
+        graph[id_list.index(report[i][0])].add(report[i][1])
+        dic[report[i][1]].append(id_list.index(report[i][0]))
+    
+
+    for i in id_list:
+        if len(dic[i]) >= k:
+            for j in dic[i]:
+                answer[j]+=1
+
+
+    return answer
