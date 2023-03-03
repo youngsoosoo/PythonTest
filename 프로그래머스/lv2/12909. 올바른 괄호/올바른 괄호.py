1db947772b744f97d1cbdb60f4ee3a50485d8c30
Young soo <lovepys01@naver.com>
2023-03-03 10:33:25 +0900

    [level 2] Title: 올바른 괄호, Time: 6.81 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,12 @@
+def solution(s):
+    cnt=0
+    for i in s:
+        if i == "(":
+            cnt+=1
+        else:
+            cnt-=1
+        if cnt < 0:
+            return False
+    if cnt != 0:
+        return False
+    return True
