b37d49d4e5fa20e72eb4d8fe48916f3fdc671548
Young soo <lovepys01@naver.com>
2023-03-06 14:12:31 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42747. H－Index/H－Index.py
@@ -0,0 +1,7 @@
+def solution(citations):
+    answer = 0
+    citations.sort()
+    for i, citation in enumerate(citations):
+        if citation >= len(citations) - i:
+            return len(citations) - i
+    return 0
