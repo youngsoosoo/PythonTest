6740225ef6c630d7777f0332835096db820d7666
Young soo <lovepys01@naver.com>
2023-03-08 15:40:01 +0900

    [level 2] Title: 튜플, Time: 0.04 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/64065. 튜플/튜플.py
@@ -0,0 +1,22 @@
+def solution(s):
+    a=''
+    result = []
+    for i in list(s[1:-1]):
+        if i.isnumeric() or i == ',':
+            a += i
+        if i == '}':
+            a = a.strip(',')
+            result.append(list(map(int, a.split(','))))
+            a = ''
+            
+    result.sort(key=len)
+    
+    if len(result) == 1:
+        return result[0]
+    answer = [result[0][0]]
+    for i in range(1, len(result)):
+        for j in result[i]:
+            if j not in result[i-1]:
+                answer.append(j)
+        
+    return answer
