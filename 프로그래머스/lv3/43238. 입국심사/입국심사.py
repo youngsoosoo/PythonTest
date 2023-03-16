5856585ae65c7d4ceb2d8a42816e28927b7a328a
Young soo <lovepys01@naver.com>
2023-03-16 15:18:52 +0900

    [level 3] Title: 입국심사, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv3/43238. 입국심사/입국심사.py
+++ 프로그래머스/lv3/43238. 입국심사/입국심사.py
@@ -1,16 +1,22 @@
 def solution(n, times):
-    start, end = 1, max(times)*n
+    answer = 0
+    start, end = min(times), max(times)*n
+    
     while start <= end:
         mid = (start + end)//2
         cnt = 0
         
         for i in times:
-            if i <= mid:
-                cnt += mid//i
+            cnt += mid//i
+            
+            if cnt >= n:
+                break
         
         if cnt < n:
             start = mid + 1
         else:
+            answer = mid
             end = mid - 1
             
-    return start
+            
+    return answer
