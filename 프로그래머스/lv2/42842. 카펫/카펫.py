2ecda213a9ea5532384330f9476fa56c3e55995a
Young soo <lovepys01@naver.com>
2023-03-22 09:49:26 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.py
+++ 프로그래머스/lv2/42842. 카펫/카펫.py
@@ -1,9 +1,11 @@
 def solution(brown, yellow):
     answer = []
-    t = brown + yellow
-    for i in range(1, t+1):
-        if t%i == 0:
-            if 2*i + 2*(t//i) - 4 == brown and t//i >= i:
-                answer.append(t//i)
+    a = brown + yellow
+    for i in range(1, int(a**1/2) + 1):
+        if a % i == 0 :
+            x = a//i
+            if x*2 + i*2 - 4 == brown and x>=i:
+                answer.append(x)
                 answer.append(i)
+        
     return answer
