d3c027c8c296470f66b6dbae022c9c23898cecd3
Young soo <lovepys01@naver.com>
2023-04-15 01:10:12 +0900

    Update ［3차］ 방금그곡.py

--- 프로그래머스/lv2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.py
+++ 프로그래머스/lv2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.py
@@ -1,22 +1,28 @@
 def solution(m, musicinfos):
-    candidate =[]
-    notes ={'C#':'H','D#':'I','F#':'J','G#':'K','A#':'L'}
-    for note,new in notes.items():
-        m = m.replace(note,new)
-    for i,musicinfo in enumerate(musicinfos):
-        start,end,name,music = musicinfo.split(',')
-        for note, new in notes.items():
-            music = music.replace(note,new)
-        start, end = list(map(int,start.split(':'))),list(map(int,end.split(':')))
-        play_time = (end[0]-start[0])*60 + end[1]-start[1]
-        if play_time < len(music):
-            music = music[:play_time]
+    answer = []
+    m = m.replace("C#", "c").replace("D#", "d").replace("F#", "f").replace("G#", "g").replace("A#", "a")
+    
+    for i in musicinfos:
+        musicinfo = i.split(',')
+        musicinfo[3] = musicinfo[3].replace("C#", "c").replace("D#", "d").replace("F#", "f").replace("G#", "g").replace("A#", "a")
+        l = len(musicinfo[3])
+        # 시작 시간
+        a = musicinfo[0].split(':')
+        # 종료 시간
+        b = musicinfo[1].split(':')
+        # 재생 시간 정의
+        t = (int(b[0])-int(a[0]))*60 + (int(b[1]) - int(a[1]))
+        if t < l:
+            an = musicinfo[3][:t]
         else:
-            music = music*(play_time//len(music)) + music[:(play_time%len(music))]
-        if m in music:
-            candidate.append([name,play_time,i])
-    candidate.sort(key=lambda x: (-x[1],x[0]))
-    if candidate:
-        return candidate[0][0]
+            an = musicinfo[3]*(t//l) + musicinfo[3][:t%l]
+
+        # 멜로디 일치 여부를 확인한 후, 실제 멜로디가 일치하는지 확인합니다.
+        if m in an:
+            answer.append([musicinfo[2], t, i])
+    answer.sort(key=lambda x: (-x[1],x[0]))
+
+    if answer:
+        return answer[0][0]
     else:
         return "(None)"
