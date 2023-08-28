18c1e9d313ae76ab1754765f32ebd6d5284325c4
Young soo <lovepys01@naver.com>
2023-08-28 15:35:29 +0900

    [Silver III] Title: N과 M (7), Time: 2144 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+def recur(number):
+
+  if number == m:
+    print(*answer)
+    return
+
+  for i in arr:
+    answer.append(i)
+    recur(number+1)
+    answer.pop()
+
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+answer=[]
+arr.sort()
+recur(0)
