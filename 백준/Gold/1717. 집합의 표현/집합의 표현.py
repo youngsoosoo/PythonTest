7c0565341e98dee7bfd562c2c6240913dede4fbf
Young soo <lovepys01@naver.com>
2023-12-26 16:31:16 +0900

    [Gold V] Title: 집합의 표현, Time: 300 ms, Memory: 71112 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.py
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.py
@@ -9,24 +9,20 @@ def _union(A, B): # 최대 높이를 확인해서 합쳐준다! 효과적으로!
 	if A == B:
 		return
 	
-	if rank[A] < rank[B]:
+	if A < B:
 		visited[A] = B
-	elif rank[B] < rank[A]:
+	elif B < A:
 		visited[B] = A
-	else:
-		visited[A] = B
-		rank[B] += 1
 
 def _find(A): # 만약에 스스로를 부모로 칭하고 있다면
 	if visited[A] == A:
 		return A
 	else:
 		visited[A] = _find(visited[A])
-		return _find(visited[A])
+		return visited[A]
 
 N, M = map(int, input().split())
 visited = [i for i in range(N+1)]
-rank = [0 for i in range(N+1)] # 노드, 
 
 for _ in range(M):
 	X, A, B = map(int, input().split())
