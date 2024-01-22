66b083c290b9a51f05aea3a31b4741405f27c38d
Young soo <lovepys01@naver.com>
2024-01-22 23:48:43 +0900

    [Silver III] Title: 한국이 그리울 땐 서버에 접속하지, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+pattern = input().split("*")
+length = len(pattern[0]) + len(pattern[1])
+
+for _ in range(n):
+	file = input()
+	if length > len(file):
+		print("NE")
+		
+	else:
+		if pattern[0] == file[:len(pattern[0])] and pattern[1] == file[-len(pattern[1]):]:
+			print("DA")
+		else:
+			print("NE")
