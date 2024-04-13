ff75d7cb573a09fd92ef57abbabfd8f97b605155
Young soo <lovepys01@naver.com>
2024-04-13 12:24:56 +0900

    [level 2] Title: 올바른 괄호, Time: 35.80 ms, Memory: 53 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Solution {
+    boolean solution(String s) {
+
+        Stack<Character> stack = new Stack<>();
+        
+        for (int i=0; i<s.length(); i++){
+            if (s.charAt(i) == '(') {
+                stack.push('(');
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                
+                stack.pop();
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
