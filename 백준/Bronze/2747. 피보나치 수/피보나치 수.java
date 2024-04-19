693a87eb7a7e82783ef3f783e5d9557ca2e663d7
Young soo <lovepys01@naver.com>
2024-04-19 14:04:06 +0900

    [Bronze II] Title: 피보나치 수, Time: 120 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		int[] dp = new int[n+1];
+		dp[0] = 0;
+		dp[1] = 1;
+		for (int i = 2; i <= n; i++) {
+			dp[i] = dp[i-1] + dp[i-2];
+		}
+		
+		System.out.println(dp[n]);
+	}
+}
