ba616ea052c65de8639e4f02d2965d64d1e3c31f
Young soo <lovepys01@naver.com>
2024-05-15 10:27:30 +0900

    [Silver II] Title: 수익, Time: 880 ms, Memory: 42072 KB -BaekjoonHub

--- 백준/Silver/4097. 수익/수익.py
+++ 백준/Silver/4097. 수익/수익.py
@@ -8,11 +8,9 @@ while True:
 		break
 	
 	dp = []
-	for _ in range(n):
-		p = int(input())
-		dp.append(p)
-	
-	for i in range(1, n):
-		dp[i] = max(dp[i-1] + dp[i], dp[i])
+	for i in range(n):
+		dp.append(int(input()))
+		if i > 0:
+			dp[i] = max(dp[i-1] + dp[i], dp[i])
 	
 	print(max(dp))
