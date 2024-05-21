20d0b958f71aa0a7db4ea5cf45a276c9abeb4de7
Young soo <lovepys01@naver.com>
2024-05-21 11:30:09 +0900

    [Gold IV] Title: 부분합, Time: 112 ms, Memory: 42204 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.py
+++ 백준/Gold/1806. 부분합/부분합.py
@@ -1,26 +1,25 @@
 import sys
-
 input = sys.stdin.readline
 
 n, s = map(int, input().split())
 arr = list(map(int, input().split()))
 
-left, right = 0, 0  # 슬라이딩 윈도우의 왼쪽과 오른쪽 인덱스
-window_sum = 0  # 슬라이딩 윈도우 내의 합
-result = sys.maxsize
+start, end = 0, 0
+result = 0
+answer = sys.maxsize
 
 while True:
-	if window_sum >= s:
-		result = min(result, right - left)  # 최소 길이 갱신
-		window_sum -= arr[left]  # 슬라이딩 윈도우 왼쪽 값을 빼주고 왼쪽 인덱스 이동
-		left += 1
-	elif right == n:  # 오른쪽 인덱스가 끝에 도달하면 종료
+	if result >= s:
+		answer = min(answer, end - start)
+		result -= arr[start]
+		start += 1
+	elif end == n:
 		break
 	else:
-		window_sum += arr[right]  # 슬라이딩 윈도우 오른쪽 값을 더해주고 오른쪽 인덱스 이동
-		right += 1
-		
-if result == sys.maxsize:
+		result += arr[end]
+		end += 1
+
+if answer == sys.maxsize:
 	print(0)
 else:
-	print(result)
+	print(answer)
