5fd741c10db6bc9182eac60627c5500d4163f636
Young soo <lovepys01@naver.com>
2024-05-28 10:48:48 +0900

    [Silver I] Title: 친구 팰린드롬, Time: 112 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15270. 친구 팰린드롬/친구 팰린드롬.py
+++ 백준/Silver/15270. 친구 팰린드롬/친구 팰린드롬.py
@@ -1,29 +1,29 @@
 import sys
 input = sys.stdin.readline
 
-N, M = map(int, input().split())
-fri = [list(map(int, input().split())) for i in range(M)]
+n, m = map(int, input().split())
+fri = [list(map(int, input().split())) for i in range(m)]
 answer = 0
 
-def dfs(n, ans, visited):
+def dfs(idx, ans, visited):
 	
 	global answer
 	
-	if n == M:
+	if idx == m:
 		answer = max(answer, ans)
 		return
 	
-	if fri[n][0] in visited or fri[n][1] in visited:
-		dfs(n+1, ans, visited)
+	if fri[idx][0] in visited or fri[idx][1] in visited:
+		dfs(idx+1, ans, visited)
 	else:
-		dfs(n+1, ans+1, visited + fri[n])
-		dfs(n+1, ans, visited)
+		dfs(idx+1, ans+1, visited + fri[idx])
+		dfs(idx+1, ans, visited)
 
 dfs(0, 0, [])
 
 answer*=2
 
-if answer < N:
+if answer < n:
 	answer+=1
 
 print(answer)
