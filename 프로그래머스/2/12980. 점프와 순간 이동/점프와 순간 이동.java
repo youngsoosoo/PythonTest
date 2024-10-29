81d305297da0fcc994fced9712633336de08c806
Young soo <lovepys01@naver.com>
2024-10-29 20:27:03 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.03 ms, Memory: 52.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int ans = 1;
+
+        while (n > 1) {
+            
+            if (n%2 == 0) {
+                n/=2;
+            } else {
+                n-=1;
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+}
