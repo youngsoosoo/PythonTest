0119a9f193526fdcf1a724e95ed3c77f52873da4
Young soo <lovepys01@naver.com>
2024-12-18 21:03:36 +0900

    [level 2] Title: [3차] 압축, Time: 20.57 ms, Memory: 83.7 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/README.md
+++ 프로그래머스/2/17684. ［3차］ 압축/README.md
@@ -1,10 +1,10 @@
 # [level 2] [3차] 압축 - 17684 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17684) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17684#) 
 
 ### 성능 요약
 
-메모리: 83 MB, 시간: 6.29 ms
+메모리: 83.7 MB, 시간: 20.57 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 18일 20:46:52
+2024년 12월 18일 21:03:34
 
 ### 문제 설명
 
--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
@@ -3,6 +3,36 @@ import java.util.*;
 class Solution {
     public int[] solution(String msg) {
         List<Integer> answer = new ArrayList<>();
+        Map<String, Integer> map = makeDict();
+        
+        // 현재 msg 문자열의 가르키고(Cursor) 있는 곳의 위치
+        int cnt = 0;
+        
+        while (cnt < msg.length()) {
+            // Key 초기화
+            String key = "";
+            
+            // 문자열을 순회하며 사전에 없는 글자 탐색
+            for (int i=cnt; i<msg.length(); i++) {
+                // 사전에 존재하지 않는 글자가 나오면 사전에 추가하고 탈출
+                if (!map.containsKey(key + msg.charAt(cnt))) {
+                    map.put(key + msg.charAt(cnt), map.size()+1);
+                    break;
+                // 사전에 존재하는 글자는 문자열에 문자를 추가하며 순회
+                } else {
+                    key += msg.charAt(cnt);
+                }
+                cnt ++;
+            }
+            // 사전에서 색인 번호를 찾아 출력
+            answer.add(map.get(key));
+        }
+        
+        return answer.stream().mapToInt(Integer::intValue).toArray();
+    }
+    
+    // 사전 제작
+    public Map<String, Integer> makeDict() {
         Map<String, Integer> map = new HashMap<>();
         
         for (int i = 1; i <= 26; i++) {
@@ -10,44 +40,6 @@ class Solution {
             map.put(String.valueOf(letter), i);
         }
         
-        if (msg.length() == 1) {
-            answer.add(map.get(msg));
-        }
-        
-        int cnt = 0;
-        
-        while (cnt < msg.length()) {
-            
-            String key = String.valueOf(msg.charAt(cnt));
-            
-            if (map.containsKey(key)) {
-                int flagCnt = 0;
-                
-                for (int j=cnt+1; j<msg.length(); j++) {
-                    flagCnt++;
-                    if (map.containsKey(key + String.valueOf(msg.charAt(j)))) {
-                        key += String.valueOf(msg.charAt(j));
-                    } else {
-                        answer.add(map.get(key));
-                        map.put(key + String.valueOf(msg.charAt(j)), map.size()+1);
-                        break;
-                    }
-                }
-                
-                if (flagCnt > 1) {
-                    cnt += flagCnt;
-                } else {
-                    cnt++;
-                }
-                
-                if (map.containsKey(msg.substring(cnt, msg.length()))) {
-                    answer.add(map.get(msg.substring(cnt, msg.length())));
-                    break;
-                }
-            }
-        }
-        
-        
-        return answer.stream().mapToInt(Integer::intValue).toArray();
+        return map;
     }
 }
