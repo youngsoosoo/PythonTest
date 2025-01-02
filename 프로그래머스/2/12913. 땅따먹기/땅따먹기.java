a9d62f484391f0b4e30e80a2029594708b616099
Young soo <lovepys01@naver.com>
2025-01-02 16:21:34 +0900

    [level 2] Title: 땅따먹기, Time: 18.00 ms, Memory: 87.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    int solution(int[][] land) {
+        int answer = 0;
+        
+        for (int i = 1; i < land.length; i++) {
+            land[i][0] += maxNumber(land[i-1][1], land[i-1][2], land[i-1][3]);
+            land[i][1] += maxNumber(land[i-1][0], land[i-1][2], land[i-1][3]);
+            land[i][2] += maxNumber(land[i-1][0], land[i-1][1], land[i-1][3]);
+            land[i][3] += maxNumber(land[i-1][0], land[i-1][1], land[i-1][2]);
+        }
+        
+        for (int i = 0; i < 4; i++) {
+            answer = Math.max(answer, land[land.length-1][i]);
+        }
+
+        return answer;
+    }
+    
+    public int maxNumber(int a, int b, int c) {
+
+        return Math.max(Math.max(a, b), c);
+    }
+}
