941354bb57a9f2b7309cd48d66f7ed64608c2780
Young soo <lovepys01@naver.com>
2025-11-30 15:21:52 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
@@ -2,15 +2,8 @@ def solution(n,a,b):
     answer = 0
     
     while abs(a-b) >= 1:
-        # 현재 N//2의 수보다 둘 다 작으면 만난다.
-        
         answer += 1
-        if a % 2 != 0:
-            a += 1
-        if b % 2 != 0:
-            b += 1
-        a //= 2
-        b //= 2
+        a, b = (a+1) // 2, (b+1) // 2
         
         
     
