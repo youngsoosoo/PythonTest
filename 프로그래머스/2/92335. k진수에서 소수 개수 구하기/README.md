b87150cab42426adb54755312e7d35e04726e9f7
Young soo <lovepys01@naver.com>
2026-03-17 20:04:20 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 106.18 ms, Memory: 9.44 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/README.md
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] k진수에서 소수 개수 구하기 - 92335 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92335) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92335?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 0.03 ms
+메모리: 9.44 MB, 시간: 106.18 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 11일 10:32:58
+2026년 03월 17일 20:04:17
 
 ### 문제 설명
 
--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
@@ -1,39 +1,34 @@
+
+answer = ''
+
 def solution(n, k):
-    answer = 0
+    result = 0
     
-    def change(n, q):
-        rev_base = ''
-
-        while n > 0:
-            n, mod = divmod(n, q)
-            rev_base += str(mod)
-
-        return rev_base[::-1]
-    
-    def is_prime(x):
-        if x < 2:
-            return False
-        for i in range(2, int(x**0.5) + 1):
-            if x % i == 0:
-                return False
-        return True
-    
-    prime = change(n, k)
-    
-    s = []
-    result = ''
-    for p in prime:
-        if p != '0':
-            s.append(p)
-        else:
-            if s:
-                result = ''.join(s)
-                if is_prime(int(result)):
-                    answer+=1
-                s=[]
+    answer = findNumber(n, k)
+    answer = answer.split('0')
+    for i in answer:
+        if len(i) == 0:
+            continue
+        if int(i) < 2:
+            continue
         
-    a = ''.join(s)
-    if len(a) > 0 and is_prime(int(a)):
-        answer+=1
+        if is_prime_number(int(i)):
+            result += 1
     
-    return answer
+    return result
+
+def is_prime_number(n):
+    end = int(n**(1/2))
+    for i in range(2, end+1):
+        if n % i == 0:
+            return False
+    
+    return True
+
+def findNumber(n, k):
+    global answer
+    while n:
+        answer += str(n % k)
+        n //= k
+    
+    return answer[::-1]
